f09ddb55d0ecbe604b266186960f7c231bfb258d
khingxc <khingkhwanc@gmail.com>
2022-05-27 10:46:24 +0700

    fix: fixing locustfile

--- benchmarks/locustfile.py
+++ benchmarks/locustfile.py
@@ -1,14 +1,18 @@
 from locust import HttpUser, task
 
+id = 0
+
 class Benchmarks(HttpUser):
 
     @task
     def paste(self):
+        global id
         _ = self.client.post(url="/api/paste", json={"title": "abcd", "content": "efgh"})
+        id += 1
 
-    # @task
-    # def id(self):
-    #     self.client.get("/api/paste")
+    @task
+    def id(self):
+        self.client.get("/api/" + str(id))
 
     @task
     def recents(self):
